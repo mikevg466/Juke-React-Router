856d1b104a7b6e6136a2f8c6c80a9d0dc4611602
Matt Connelly <matthew.connelly9@gmail.com>
2017-05-16 17:17:45 -0400

    placing the picture in the frame

--- browser/react/index.js
+++ browser/react/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
 import {Router, Route, hashHistory, browserHistory} from 'react-router';
+import Albums from './components/Albums'
 
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={AppContainer} >
+    <Route path='/' component={Albums} />
     </Route>
   </Router>,
   document.getElementById('app')
