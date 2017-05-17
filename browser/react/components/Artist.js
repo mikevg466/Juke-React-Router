8dc3ef5918050185d83e39e5d2f88561327c20f1
Matt Connelly <matthew.connelly9@gmail.com>
2017-05-17 12:19:24 -0400

    album tab working in artists

--- browser/react/components/Artist.js
+++ browser/react/components/Artist.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Albums from './Albums'
 import Songs from './Songs'
-
-
+import {Link} from 'react-router'
 
 export default class extends React.Component{
   componentDidMount(){
@@ -10,20 +9,43 @@ export default class extends React.Component{
   }
 
   render(){
+
+    const selectedArtist = this.props.selectedArtist;
+    const children = this.props.children;
+    const propsToPassToChildren = { 
+          albums: this.props.selectedArtist.albums || [],
+          songs: this.props.selectedArtist.songs || [],
+          currentSong: this.props.currentSong,
+          isPlaying: this.props.isPlaying,
+          toggleOne: this.props.toggleOne
+      /**todo: make sure to include all the props that the child components need! **/
+    }
+
     return (
-      <div>
-        <h3>{this.props.selectedArtist.name}</h3>
-        <Albums albums= {
-          this.props.selectedArtist.albums || [] } />
-        <Songs songs= {
-          this.props.selectedArtist.songs || [] } 
-          currentSong={this.props.currentSong}
-          isPlaying={this.props.isPlaying}
-          toggleOne={this.props.toggleOne}
-          />
-        }
-      </div>
+    <div>
+      <h3>{ selectedArtist.name }</h3>
+      <ul className="nav nav-tabs">
+        <li><Link to={`/artist/${this.props.selectedArtist.id}/albums`}>ALBUMS</Link></li>
+        <li><Link to={`/artist/${this.props.selectedArtist.id}/songs`}>SONGS</Link></li>
+      </ul>
+      { children && React.cloneElement(children, propsToPassToChildren) }
+    </div>
     )
+
+    // return (
+    //   <div>
+    //     <h3>{this.props.selectedArtist.name}</h3>
+    //     <Albums albums= {
+    //       this.props.selectedArtist.albums || [] } />
+    //     <Songs songs= {
+    //       this.props.selectedArtist.songs || [] } 
+    //       currentSong={this.props.currentSong}
+    //       isPlaying={this.props.isPlaying}
+    //       toggleOne={this.props.toggleOne}
+    //       />
+    //     }
+    //   </div>
+    // )
   }
 }
 
--- browser/react/index.js
+++ browser/react/index.js
@@ -14,7 +14,11 @@ ReactDOM.render(
       <Route path='/albums' component={Albums} />
       <Route path='/albums/:albumId' component={Album} />
       <Route path='/artists' component={Artists} />
-      <Route path='/artists/:artistId' component={Artist} />
+      <Route path='/artists/:artistId' component={Artist}> 
+        <Route path='/artist/:artistId/albums' component={Albums}/>
+      </Route>
+
+  
     </Route>
   </Router>,
   document.getElementById('app')
