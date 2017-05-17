4d7c66af98e27c15705f7b27584b708f802f4941
Matt Connelly <matthew.connelly9@gmail.com>
2017-05-17 12:04:38 -0400

    got it working

--- browser/react/components/Artist.js
+++ browser/react/components/Artist.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Albums from './Albums'
+import Songs from './Songs'
+
 
 
 export default class extends React.Component{
@@ -10,21 +13,33 @@ export default class extends React.Component{
     return (
       <div>
         <h3>{this.props.selectedArtist.name}</h3>
-        <h4>Albums</h4>
-        {
-          this.props.selectedArtist.albums ?
-            this.props.selectedArtist.albums
-              .map(album => <h4>{album.name}</h4>)
-            : null
-        }
-        <h4>Songs</h4>
-        {
-          this.props.selectedArtist.songs ?
-            this.props.selectedArtist.songs
-              .map(song => <h4>{song.name}</h4>)
-            : null
+        <Albums albums= {
+          this.props.selectedArtist.albums || [] } />
+        <Songs songs= {
+          this.props.selectedArtist.songs || [] } 
+          currentSong={this.props.currentSong}
+          isPlaying={this.props.isPlaying}
+          toggleOne={this.props.toggleOne}
+          />
         }
       </div>
     )
   }
 }
+
+
+// <h4>Albums</h4>
+//         { 
+//           this.props.selectedArtist.albums ?
+//             this.props.selectedArtist.albums
+//               .map(album => <h4>{album.name}</h4>)
+//             : null
+//         }
+
+//  <h4>Songs</h4>
+//         {
+//           this.props.selectedArtist.songs ?
+//             this.props.selectedArtist.songs
+//               .map(song => <h4>{song.name}</h4>)
+//             : null
+//         }
